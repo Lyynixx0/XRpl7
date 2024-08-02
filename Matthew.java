ac28ca2c4abc4fd2cd2fc2ee39c1c11a1ed239c0
Lynixx <matthew_widodo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-02 13:59:28 +0700

    Add files via upload

---
+++ Matthew.java
@@ -0,0 +1,6 @@
+public class skibidi {
+    public static void main(String[] args ){
+        System.out.println("Nama saya Matthew Immanuel Widodo, saya dari kelas XR7, asal kota dari Malang,Singosari");
+        String a = "1";
+    }  
+}
