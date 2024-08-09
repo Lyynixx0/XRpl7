adb7a3a80b371f43eeec3ec67d0a10ce86aced97
Lynixx <matthew_widodo_33rpl@student.smktelkom-mlg.sch.id>
2024-08-09 14:26:15 +0700

    Add files via upload

---
+++ test.java
@@ -0,0 +1,6 @@
+public class test{
+    public static void main(String[] args)  {
+     System.out.println("Nama saya Matthew Immanuel Widodo Saya tingal di Malang, Umur saya 15 "); 
+    String a = "1"; 
+    }
+}
